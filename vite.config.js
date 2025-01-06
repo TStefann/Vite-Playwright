c8f5647bcb19aaed70c9e95f8395ea3e9b963137
TStefan <taraipanstefan97@gmail.com>
2025-01-06 14:46:34 +0100

    add base for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/playwright",
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
-    css:true,
-      // reporters: ['json'],
-     outputFile: 'test.json'
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+    css: true,
+    // reporters: ["json"],
+    outputFile: "test.json",
   },
 });
 
-
-// docker build -t my-vite-project .   
-// docker run --rm -v ./test.json:/app/test.json my-vite-project          
+// docker build -t my-vite-project .
+// docker run --rm -v ./test.json:/app/test.json my-vite-project
 
 // npx playwright test --ui
 
-// 
+//
